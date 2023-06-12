05916e1982c8866266fb9666eb0e81193b811483
Timm Maximilian Wirtz <tmw.contact@protonmail.com>
2023-06-12 15:54:58 +0200

    Update

--- functions/utils.py
+++ functions/utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 from functions.widgets import *
 
 
-def get_measurement_data(api_key, sampling_feature_id):
+def get_measurement_data(api_key, sampling_feature_id, selected_keys):
     base_url = f"https://api-test.georoc.eu/api/v1/queries/fulldata/{sampling_feature_id}"
     headers = {
         "accept": "application/json",
@@ -15,8 +15,8 @@ def get_measurement_data(api_key, sampling_feature_id):
     if response.status_code == 200:
         data = json.loads(response.text)['Data'][0]
 
-        # Create a dictionary containing all keys and values from data
-        df_data = {key: data[key] for key in keys}
+        # Create a dictionary containing only the selected keys and their values from data
+        df_data = {key: data[key] for key in selected_keys}
 
         # Convert all values in df_data to lists
         for key, value in df_data.items():
@@ -39,6 +39,32 @@ def get_measurement_data(api_key, sampling_feature_id):
     else:
         print(f"Error fetching data for sample ID {sampling_feature_id}, Status code: {response.status_code}")
         return None
+
+
+# Iterate over the list of SamplingFeatureIDs
+for index, sampling_feature_id in enumerate(sampling_feature_ids):
+    print(f"Fetching measurement data for SamplingFeatureID: {sampling_feature_id}")
+
+    # Get the selected keys from the checkboxes
+    selected_keys = [checkboxes[i].description for i in range(len(checkboxes)) if checkboxes[i].value]
+
+    # Get the measurement data for the current SamplingFeatureID using the selected keys
+    df = get_measurement_data(my_app.api.api_key, sampling_feature_id, selected_keys)
+
+    # Check if the dataframe is not empty and not None
+    if df is not None and not df.empty:
+        # Append the dataframe to the measurement_data DataFrame
+        measurement_data = measurement_data._append(df, ignore_index=True)
+
+        # Print the dataframe
+        print(f"Data for SamplingFeatureID {sampling_feature_id}:\n", df)
+    elif df is None:
+        print(f"Error occurred while fetching data for SamplingFeatureID {sampling_feature_id}")
+    else:
+        print(f"No measurement data found for SamplingFeatureID {sampling_feature_id}")
+
+    # Update the progress bar
+    progress_bar.value = index + 1
 
 
 
