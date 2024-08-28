eaf8a18145d127b1dd537bcb8279f50705957dbc
mk618 <marthe.kloecking@cantab.net>
2024-08-28 19:12:02 +0200

    fix functions/utils.py after API updates; add option for new API endpoint "/download/filtered"

--- functions/utils.py
+++ functions/utils.py
@@ -7,8 +7,8 @@ from IPython.display import display, HTML
 import ipywidgets as widgets
 
 
-def get_measurement_data(api_key, sampling_feature_id, selected_keys):
-    base_url = f"https://api-test.georoc.eu/api/v1/queries/fulldata"
+def get_measurement_data(api_key, sampling_feature_id, selected_keys_data, selected_keys_metadata):
+    base_url = f"https://api-test.georoc.eu/api/v1/queries/fulldata/{sampling_feature_id}"
     headers = {
         "accept": "application/json",
         "DIGIS-API-ACCESSKEY": api_key
@@ -16,23 +16,18 @@ def get_measurement_data(api_key, sampling_feature_id, selected_keys):
 
     response = requests.get(base_url, headers=headers)
     if response.status_code == 200:
-        data = json.loads(response.text)['Data'][0]
+        data = json.loads(response.text)['batchData'][0]['results']
+        metadata = json.loads(response.text)
 
-        # Create a dictionary containing only the selected keys and their values from data
-        df_data = {key: data[key] for key in selected_keys}
-
-        # Convert all values in df_data to lists
-        for key, value in df_data.items():
-            if not isinstance(value, list):
-                df_data[key] = [value]
-
-        # Find the longest list in df_data
-        max_len = max([len(value) for value in df_data.values()])
-
-        # Extend all the lists to the length of the longest list
-        for key, value in df_data.items():
-            if len(value) < max_len:
-                df_data[key] = value + [None] * (max_len - len(value))
+        # Create list of dictionaries containing only the selected keys and their values from data
+        #df_data = {key: data[key] for key in selected_keys_data}
+        df_metadata = {key: metadata[key] for key in selected_keys_metadata}
+        
+        df_data = []
+        for l in data:
+            data_entry = {key: l[key] for key in selected_keys_data}
+            df_entry = df_metadata | data_entry
+            df_data.append(df_entry)
 
         # Create the DataFrame
         df = pd.DataFrame(df_data)
